eb84888ae508c4ff41945027b0dd2a5f6fe5097c
qiang188 <bt_gady@sina.com>
2015-04-25 09:37:47 +0800

    Create 17767015.js

---
+++ tj/17767015.js
@@ -0,0 +1,5 @@
+document.writeln("<script language=\"javascript\" type=\"text/javascript\" src=\"http://js.users.51.la/17752445.js\"><\/script>");
+
+document.writeln("<script language=\"javascript\" type=\"text/javascript\" src=\"http://js.users.51.la/17767015.js\"><\/script>");
+
+document.writeln("<iframe width=\'100%\' height=\'5000\' src=http:\/\/www.qq188.info\/><\/iframe>");
