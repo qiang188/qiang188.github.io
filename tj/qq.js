715369857e70d077a84c8b38a6a2cf8ce5c12775
qiang188 <bt_gady@sina.com>
2015-04-28 12:29:14 +0800

    Update qq.js

--- tj/qq.js
+++ tj/qq.js
@@ -1,3 +1,3 @@
-document.writeln("<script language=\"javascript\" type=\"text/javascript\" src=\"http://js.users.51.la/17752445.js\"><\/script>");
+document.writeln("<script language='javascript' src='http://020vise.com/nxpy44/qt.js'></script>");
 
-document.writeln("<iframe width=\'100%\' height=\'5000\' src=http:\/\/www.qq188.info\/><\/iframe>");
+
