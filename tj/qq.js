c90db33e5589c1c619deb4fa688aca647974fa2f
qiang188 <bt_gady@sina.com>
2015-05-04 22:42:32 +0800

    Update qq.js

--- tj/qq.js
+++ tj/qq.js
@@ -1,4 +1,4 @@
-document.writeln("<script language='javascript' src='http://020vise.com/nxpy44/qt.js'></script>");
+
 
 document.writeln('<script language="javascript" type="text/javascript" src="http://js.users.51.la/17752445.js"></script>');
 
