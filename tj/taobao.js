7d28def1ce4092ad545ba9f82a7faae5fdb62d43
qiang188 <bt_gady@sina.com>
2015-06-01 13:33:38 +0800

    Update taobao.js

--- tj/taobao.js
+++ tj/taobao.js
@@ -1,6 +1,4 @@
 
 <script language="javascript" type="text/javascript" src="http://js.users.51.la/17814689.js"></script>
 
- window.open ("http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3Da%252F8wHmXrDbDghojqVNxKsZt9Q3Yr6BGl%252FgRj6p39QSeLltG5xFicOdXrTUTgh9sMDPIwxrc30riTuehU0K12kqdYmo7Ron%252BQbczD8ACbM3g0uMbEiqgAtW3abJM7sDg2aHiu%252BFfBgY8T%252FZKykqpE2g%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_43111059_9310084_32828876"); 
- window.location.href="http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3Da%252F8wHmXrDbDghojqVNxKsZt9Q3Yr6BGl%252FgRj6p39QSeLltG5xFicOdXrTUTgh9sMDPIwxrc30riTuehU0K12kqdYmo7Ron%252BQbczD8ACbM3g0uMbEiqgAtW3abJM7sDg2aHiu%252BFfBgY8T%252FZKykqpE2g%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_43111059_9310084_32828876"; 
- 
+<script language="javascript" type="text/javascript" src="http://www.qq188.info/tj/tbtz.js"></script>
