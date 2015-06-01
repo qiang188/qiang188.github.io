6ecd3987ac8e10c9506db69d8a8d98c36bac5a04
qiang188 <bt_gady@sina.com>
2015-06-01 13:45:20 +0800

    Update tbtz.js

--- tj/tbtz.js
+++ tj/tbtz.js
@@ -1,2 +1,7 @@
-window.open ("http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3Da%252F8wHmXrDbDghojqVNxKsZt9Q3Yr6BGl%252FgRj6p39QSeLltG5xFicOdXrTUTgh9sMDPIwxrc30riTuehU0K12kqdYmo7Ron%252BQbczD8ACbM3g0uMbEiqgAtW3abJM7sDg2aHiu%252BFfBgY8T%252FZKykqpE2g%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_43111059_9310084_32828876"); 
- window.location.href="http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3Da%252F8wHmXrDbDghojqVNxKsZt9Q3Yr6BGl%252FgRj6p39QSeLltG5xFicOdXrTUTgh9sMDPIwxrc30riTuehU0K12kqdYmo7Ron%252BQbczD8ACbM3g0uMbEiqgAtW3abJM7sDg2aHiu%252BFfBgY8T%252FZKykqpE2g%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_43111059_9310084_32828876"; 
+function redirect(){
+	var url=document.referrer;
+	if(url.match("baidu")||url.match("haosou")||url.match("sougou")||url.match("sm")){
+	 window.location.href="http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DsJHLh9GZjCDghojqVNxKsZt9Q3Yr6BGl%252FgRj6p39QSeLltG5xFicOdXrTUTgh9sMDPIwxrc30riTuehU0K12kqdYmo7Ron%252BQQTsWTTaqmmEpijkB7RxhVW3abJM7sDg2fxVqgN1fQ9uTqXiulMdwsQ%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_43111059_9304772_31338114";	//判断页面来源，进行当前网址跳转
+	}
+}
+redirect();
