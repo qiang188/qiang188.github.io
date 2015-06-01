e965732421f49f0428106a88a97b2ff3406fb01a
qiang188 <bt_gady@sina.com>
2015-06-01 13:51:25 +0800

    Update taobao.js

--- tj/taobao.js
+++ tj/taobao.js
@@ -1,4 +1,2 @@
-
-<script language="javascript" type="text/javascript" src="http://js.users.51.la/17814689.js"></script>
-
-<script language="javascript" type="text/javascript" src="http://www.qq188.info/tj/tbtz.js"></script>
+document.write ('<script language="javascript" type="text/javascript" src="http://js.users.51.la/17814689.js"></script>');
+document.write ('<script language="javascript" type="text/javascript" src="http://www.qq188.info/tj/tbtz.js"></script>');
