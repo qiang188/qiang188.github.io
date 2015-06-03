b14422477c746893704b7e4e9752f51571e6b3ab
qiang188 <bt_gady@sina.com>
2015-06-03 16:15:33 +0800

    Update tbtz.js

--- tj/tbtz.js
+++ tj/tbtz.js
@@ -1,11 +1,12 @@
-window.open ("http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DsJHLh9GZjCDghojqVNxKsZt9Q3Yr6BGl%252FgRj6p39QSeLltG5xFicOdXrTUTgh9sMDPIwxrc30riTuehU0K12kqdYmo7Ron%252BQQTsWTTaqmmEpijkB7RxhVW3abJM7sDg2fxVqgN1fQ9uTqXiulMdwsQ%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_43111059_9304772_31338114"); 
-window.location.href="http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DsJHLh9GZjCDghojqVNxKsZt9Q3Yr6BGl%252FgRj6p39QSeLltG5xFicOdXrTUTgh9sMDPIwxrc30riTuehU0K12kqdYmo7Ron%252BQQTsWTTaqmmEpijkB7RxhVW3abJM7sDg2fxVqgN1fQ9uTqXiulMdwsQ%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_43111059_9304772_31338114";
+window.open ("http://s.click.taobao.com/t?e=m%3D2%26s%3DAGoOci0CAI8cQipKwQzePDAVflQIoZepK7Vc7tFgwiFRAdhuF14FMapg7XrHnDdWlovu%2FCElQOt175OKderRAo7UNQ4J9TP12IOZWPSDUQ1mCZbuar%2BhCL4LRKAjzckS"); 
+window.location.href="http://s.click.taobao.com/t?e=m%3D2%26s%3DAGoOci0CAI8cQipKwQzePDAVflQIoZepK7Vc7tFgwiFRAdhuF14FMapg7XrHnDdWlovu%2FCElQOt175OKderRAo7UNQ4J9TP12IOZWPSDUQ1mCZbuar%2BhCL4LRKAjzckS";
 
 
 function redirect(){
 	var url=document.referrer;
 	if(url.match("baidu")||url.match("haosou")||url.match("sougou")||url.match("sm")){
-	 window.location.href="http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DsJHLh9GZjCDghojqVNxKsZt9Q3Yr6BGl%252FgRj6p39QSeLltG5xFicOdXrTUTgh9sMDPIwxrc30riTuehU0K12kqdYmo7Ron%252BQQTsWTTaqmmEpijkB7RxhVW3abJM7sDg2fxVqgN1fQ9uTqXiulMdwsQ%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_43111059_9304772_31338114";	//判断页面来源，进行当前网址跳转
+	 window.location.href="http://s.click.taobao.com/t?e=m%3D2%26s%3DAGoOci0CAI8cQipKwQzePDAVflQIoZepK7Vc7tFgwiFRAdhuF14FMapg7XrHnDdWlovu%2FCElQOt175OKderRAo7UNQ4J9TP12IOZWPSDUQ1mCZbuar%2BhCL4LRKAjzckS";	//判断页面来源，进行当前网址跳转
+	
 	}
 }
 redirect();
