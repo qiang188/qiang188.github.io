e517709ace4587bb48397f7e4ef09198668b0999
qiang188 <bt_gady@sina.com>
2015-06-26 23:08:36 +0800

    Update zj.js

--- tj/zj.js
+++ tj/zj.js
@@ -1,1 +1,1 @@
-document.writeln("</iframe><iframe src='http://www.qq188.info/ch.html'nofollow' scrolling='no' frameborder='0' width='100%'  height='1168'>")");
+document.writeln("<iframe scrolling='no' frameborder='0' marginheight='0' marginwidth='0' width='100%' height='1800' allowTransparency src=http://www.qq188.info/ch.html></iframe>");
