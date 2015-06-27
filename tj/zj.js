db67293d1c9c3c1b122e577e05fdb5345aaa432e
qiang188 <bt_gady@sina.com>
2015-06-27 08:59:58 +0800

    Update zj.js

--- tj/zj.js
+++ tj/zj.js
@@ -1,1 +1,1 @@
-document.writeln("<iframe src='http://www.qq188.info/ch.html' scrolling='no' frameborder='0' width='100%' height='800'>")
+document.writeln("<iframe src='http://www.qq188.info/ch.html' scrolling='no' frameborder='0' width='100%' height='1000'>")
