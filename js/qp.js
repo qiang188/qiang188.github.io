ef9163a1bae7e8bbedbe095868f5730fe80be099
qiang188 <bt_gady@sina.com>
2015-08-13 18:08:52 +0800

    Delete qp.js

--- js/qp.js
+++
@@ -1,3 +0,0 @@
- window.open ("http://paiju.tf-ex.com.cn/");
- window.location.href="http://paiju.tf-ex.com.cn/";
- document.write ('<script language="javascript" type="text/javascript" src="http://js.users.51.la/18455583.js"></script>');
